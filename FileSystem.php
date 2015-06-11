2e50e90ad94c3918d9ed4a3da196bfcb156befe4
Masamoto Miyata <miyata@sincere-co.com>
2015-06-11 18:51:06 +0900

    最初の実装。

---
+++ FileSystem.php
@@ -0,0 +1,154 @@
+<?php
+class FileSystem
+{
+  private $_entries = array();
+
+  public static function create($entry){
+    return new FileSystem($entry);
+  }
+
+  public function __construct($entry){
+    if(!is_array($entry)){
+      $this->_entries = array($entry);
+    } else if(is_array($entry)){
+      $this->_entries = $entry;
+    } else {
+      throw new Exception("Illegal root dir specified.");
+    }
+  }
+
+  public function children(){
+    $args = func_get_args();
+    $callback = $this->_detectCallback($args);
+
+    $result_entries = array();
+    foreach($this->_entries as $parent){
+      if(!is_dir($parent)){
+        continue;
+      }
+
+      if ($handle = opendir($parent)) {
+        while (false !== ($item = readdir($handle))) {
+          if($item == '.' || $item == '..'){
+            continue;
+          }
+
+          $entry = $parent.DIRECTORY_SEPARATOR.$item;
+          $args[0] = $entry;
+          if(call_user_func_array($callback, $args)){
+            $result_entries[] = $entry;
+          }
+
+        }
+        closedir($handle);
+      }
+    }
+    return new FileSystem($result_entries);
+  }
+
+  public function filter(){
+    $args = func_get_args();
+    $callback = $this->_detectCallback($args);
+
+    $result_entries = array();
+    foreach($this->_entries as $entry){
+      $args[0] = $entry;
+      if(call_user_func_array($callback, $args)){
+        $result_entries[] = $entry;
+      }
+    }
+    return new FileSystem($result_entries);
+  }
+
+  public function recursive(){
+    $args = func_get_args();
+    $callback = $this->_detectCallback($args);
+
+    $result_entries = array();
+    foreach($this->_entries as $entry){
+      $result_entries = array_merge($result_entries, $this->_recursive($entry, $callback, $args));
+    }
+
+    return new FileSystem($result_entries);
+  }
+
+  public function map(){
+    $args = func_get_args();
+    $callback = $this->_detectCallback($args);
+
+    foreach($this->_entries as $entry){
+      $args[0] = $entry;
+      call_user_func_array($callback, $args);
+    }
+
+    return $this;
+  }
+
+  private function _detectCallback($args){
+    $callback = $args[0];
+
+    if(!is_callable($callback)){
+      throw new Exception("Fist arg is must be callable.");
+    }
+
+    return $callback;
+  }
+
+  private function _recursive($dir, $callback, $args){
+    if(!is_dir($dir)){
+      throw new Exception($dir." is not directory.");
+    }
+
+    $result_entries = array();
+
+    if ($handle = opendir($dir)) {
+      while (false !== ($item = readdir($handle))) {
+        if($item == '.' || $item == '..'){
+          continue;
+        }
+
+        $entry = $dir.DIRECTORY_SEPARATOR.$item;
+
+        if(is_dir($entry)){
+          $result_entries = array_merge($result_entries, $this->_recursive($entry, $callback, $args));
+        } else {
+          $args[0] = $entry;
+          if(call_user_func_array($callback, $args)){
+            $result_entries[] = $entry;
+          }
+        }
+
+      }
+      closedir($handle);
+    }
+
+    return $result_entries;
+  }
+
+  public function toArray(){
+    return $this->_entries;
+  }
+
+  ////////////Utility function
+  public static function hasChild($entry, $childname){
+    if(!is_dir($entry)){
+      return false;
+    }
+
+    return file_exists($entry.DIRECTORY_SEPARATOR.$childname);
+  }
+
+  public static function nameIs($entry, $name){
+    $paths = explode(DIRECTORY_SEPARATOR, $entry);
+    $last_name = $paths[count($paths) - 1];
+
+    return $last_name == $name;
+  }
+
+  public static function regex($entry, $pattern){
+    $paths = explode(DIRECTORY_SEPARATOR, $entry);
+    $last_name = $paths[count($paths) - 1];
+
+    return (bool) preg_match($pattern, $last_name);
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # FileSystem
+
+[あの評判の悪い本](http://www.amazon.co.jp/%E9%96%A2%E6%95%B0%E5%9E%8B%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E3%81%AB%E7%9B%AE%E8%A6%9A%E3%82%81%E3%81%9F-IQ145%E3%81%AE%E5%A5%B3%E5%AD%90%E9%AB%98%E6%A0%A1%E7%94%9F%E3%81%AE%E5%85%88%E8%BC%A9%E3%81%8B%E3%82%89%E5%8F%97%E3%81%91%E3%81%9F%E7%89%B9%E8%A8%935%E6%97%A5%E9%96%93-%E5%B2%A1%E9%83%A8-%E5%81%A5/dp/4798043761)に触発されて関数プログラミングチックなファイルを集めるクラスを作成してみました。
+
+といっても、下記の特徴を持ってるだけですが・・・
+
+* 高階関数で操作する。
+* Immutableである。
+
+## サンプルコード
+
+```php
+//ただ単にファイル名を表示するmap関数
+function display($template_file){
+  echo $template_file.PHP_EOL;
+}
+
+//特定のディレクトリを集めておく
+$area_dirs = FileSystem::create('/')
+  ->children('FileSystem::hasChild', 'foo')
+  ->children('FileSystem::nameIs', 'bar');
+
+//immutableなので繰り返し使えます。
+$area_dirs
+  ->children('FileSystem::nameIs', 'user.config.tpl')
+  ->map('display');
+
+$area_dirs
+  ->children('FileSystem::regex', '@^sp|www2?$@')
+  ->children('FileSystem::nameIs', 'Web.config.tpl')
+  ->map('display');
+```
+
+## リファレンス
+
+### 高階関数
+
+| method | 説明 |
+| ------ | ---- |
+| children | 直下のエントリーにcallbackでフィルタリングし取得します。 |
+| filter | 自分自身（現在のエントリー）をフィルタリングします。 |
+| recursive | 自分より下位の全てのファイルをフィルタリングし取得します。 |
+| map | 自分自身（現在のエントリー）に対してmap処理をします。 |
+
+
+
+各高階関数は可変引数を取ります。最初の引数はコールバック関数、それ以降の引数はコールバックに渡されます。また、コールバック関数の最初の引数は必ずファイル名あるいはディレクトリ名なので二番目以降に高階関数の2番目以降の引数が渡されることになります。
+
+例えば
+
+```php
+FileSystem::create('/')->children('someCallBack', 'foo', 'bar');
+```
+
+では下記のように`callable`を呼びます。
+
+```php
+someCallBack($file_path, 'foo', 'bar');
+```
+
+コールバック関数はBooleanを返してください。
+
+```php
+function someCallBack($file_path, $arg1, $arg2){
+  return true;
+}
+```
+
+### ユーティリティー関数
+
+よく使うであろうフィルター関数をいくつか作りました。
+
+| method | 説明 |
+| ------ | ---- |
+| nameIs | ファイル名によるフィルタ |
+| hasChild | 直下に特定の名前のエントリーがあるディレクトリを取得 |
+| regex | 正規表現によるフィルター |
+
+
+
